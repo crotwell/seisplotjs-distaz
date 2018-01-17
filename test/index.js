29a16e2aaf96ca63822d49b463ef5330e8da8322
Philip Crotwell <crotwell@seis.sc.edu>
2018-01-17 15:34:55 -0500

    revert to qunit 1.0 style tests

--- test/index.js
+++ test/index.js
@@ -1,21 +1,19 @@
-import test from 'ava';
-import 'babel-core/register'
-import * as distaz from '../src/distaz.js';
+require('../lib/distaz.js');
 
-test("deg km convert", t => {
-  t.is(distaz.kmtodeg(distaz.degtokm(45)), 45, "45 deg round trip");
+QUnit.test("deg km convert", function (assert) {
+  assert.equal(kmtodeg(degtokm(45)), 45, "45 deg round trip");
 });
 
-test("basic dist south pole", t => {
-    var result = distaz.distaz(0, 0, -90, 0);
-    t.is(result.delta, 90, "southpole dist");
-    t.is(result.az, 0, "south pole backazimuth");
-    t.is(result.baz, 180, "southpole azimuth");
+QUnit.test("basic dist south pole", function (assert) {
+    var result = distaz(0, 0, -90, 0);
+    assert.equal(result.delta, 90, "southpole dist");
+    assert.equal(result.az, 0, "south pole backazimuth");
+    assert.equal(result.baz, 180, "southpole azimuth");
 });
 
-test("basic dist north pole", t => {
-    var result = distaz.distaz(0, 0, 90, 0);
-    t.is(result.delta, 90, "northpole dist");
-    t.is(result.az, 180, "north pole backazimuth");
-    t.is(result.baz, 0, "north pole azimuth");
+QUnit.test("basic dist north pole", function (assert) {
+    var result = distaz(0, 0, 90, 0);
+    assert.equal(result.delta, 90, "northpole dist");
+    assert.equal(result.az, 180, "north pole backazimuth");
+    assert.equal(result.baz, 0, "north pole azimuth");
 });
